808b3ba524dc0b2fb7fcf559973a5cf39060d50c
Samuel Susetia Davilantinov Nugraha <samuelsdavilantinovn@gmail.com>
2022-10-19 15:30:55 +0700

    tanpaV1

--- cores/main.js
+++ cores/main.js
@@ -2,6 +2,6 @@ const AdminController = require("../apps/admins/controller")
 const UserController = require("./controller")
 
 module.exports = (app) => {
-  app.use("/v1/login", UserController)
-  app.use("/v1/admin", AdminController)
+  app.use("/login", UserController)
+  app.use("/admin", AdminController)
 }
