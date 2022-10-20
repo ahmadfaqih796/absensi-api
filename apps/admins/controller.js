979c105074b9de0216abd6377d8de0e197520a0c
Samuel Susetia Davilantinov Nugraha <samuelsdavilantinovn@gmail.com>
2022-10-21 01:58:38 +0700

    ValidasiFormatWaktu_CRUDUser

--- apps/admins/controller.js
+++ apps/admins/controller.js
@@ -22,7 +22,7 @@ AdminController.post("/register", async (req, res) => {
   let bulan = String(date.getMonth() + 1).padStart(2, "0")
   let tahunbulan = tahun + bulan
   let nik = await generateNIK(tahunbulan, KaryawanModel)
-  let statusJamMasuk = "08:00:00", statusJamKeluar = "17:00:00"
+  let statusJamMasuk, statusJamKeluar
   const newKaryawan = new KaryawanModel({
     username: req.body.username,
     name: req.body.name,
@@ -43,8 +43,14 @@ AdminController.post("/register", async (req, res) => {
   if (req.body.jamMasuk) {
     statusJamMasuk = await cekFormatJam(req.body.jamMasuk)
   }
+  else {
+    statusJamMasuk = true
+  }
   if (req.body.jamKeluar) {
     statusJamKeluar = await cekFormatJam(req.body.jamKeluar)
+  }
+  else {
+    statusJamKeluar = true
   }
   if (statusJamMasuk && statusJamKeluar) {
     await newKaryawan.save()
@@ -72,23 +78,43 @@ AdminController.get("/search/:nik", async (req, res) => {
 
 //untuk fitur update data karyawan untuk admin
 AdminController.put("/search/:nik", async (req, res) => {
+  let statusJamMasuk, statusJamKeluar
   let nik = handleNIK(req, res)
   const passwordSalt = await makePassword(req.body.password)
-  await KaryawanModel.findOneAndUpdate({ nik }, ({
-    username: req.body.username,
-    name: req.body.name,
-    gender: req.body.gender,
-    departemen: req.body.departemen,
-    isSPV: req.body.isSPV,
-    isAdmin: req.body.isAdmin,
-    phone: req.body.phone,
-    alamat: req.body.alamat,
-    nik: nik,
-    isActive: req.body.isActive,
-    ...passwordSalt
-  }))
-  let data = await KaryawanModel.findOne({ nik }, { password: 0, salt: 0 })
-  return res.status(200).json({ data })
+  if (req.body.jamMasuk) {
+    statusJamMasuk = await cekFormatJam(req.body.jamMasuk)
+  }
+  else {
+    statusJamMasuk = true;
+  }
+  if (req.body.jamKeluar) {
+    statusJamKeluar = await cekFormatJam(req.body.jamKeluar)
+  }
+  else {
+    statusJamKeluar = true;
+  }
+
+  if (statusJamMasuk && statusJamKeluar) {
+    await KaryawanModel.findOneAndUpdate({ nik }, ({
+      username: req.body.username,
+      name: req.body.name,
+      gender: req.body.gender,
+      departemen: req.body.departemen,
+      isSPV: req.body.isSPV,
+      isAdmin: req.body.isAdmin,
+      phone: req.body.phone,
+      alamat: req.body.alamat,
+      isActive: req.body.isActive,
+      jamMasuk: req.body.jamMasuk,
+      jamKeluar: req.body.jamKeluar,
+      ...passwordSalt
+    }))
+    let data = await KaryawanModel.findOne({ nik }, { password: 0, salt: 0 })
+    return res.status(200).json({ data })
+  }
+  else {
+    return res.status(400).json({ message: "Format Jam Tidak Sesuai <hh:mm:ss>" })
+  }
 })
 
 //Untuk fitur hapus entry data karyawan buat admin
